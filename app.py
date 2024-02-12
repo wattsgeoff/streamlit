fa580c36939b0e927fbadf8aa1a55cb8c8bad6ed
geoffkeyrock <geoff@keyrock.eu>
2024-02-12 15:29:48 -0700

    test

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Hello world")
-st.write("Hello world Again")
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 
-test = pd.DataFrame({'One':[1,2,3,4,5],'Two':[6,8,7,8,9]})
-st.write(test)
+st.write("# Welcome to Streamlit! 👋")
 
-st.plot(test)
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
